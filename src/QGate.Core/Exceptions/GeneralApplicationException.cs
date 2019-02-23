101cd23f2b1f43d4fbad2bbfcbc2e93b10845bff
FilX <barusad@digitalmail.info>
2019-02-23 07:21:50 +0100

    Added Exception helpers

---
+++ src/QGate.Core/Exceptions/GeneralApplicationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace QGate.Core.Exceptions
+{
+    /// <summary>
+    /// Serves as the base class for general application-defined exceptions.
+    /// </summary>
+    public class GeneralApplicationException : Exception
+    {
+        public GeneralApplicationException()
+        {
+        }
+
+        public GeneralApplicationException(string message) : base(message)
+        {
+        }
+
+        public GeneralApplicationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ src/QGate.Core/Exceptions/Throw.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace QGate.Core.Exceptions
+{
+    public static class Throw
+    {
+        public static void IfNull(object paramValue, string paramName)
+        {
+            if (paramValue == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+
+        public static void IfNullOrWhiteSpace(string paramValue, string paramName)
+        {
+            if (string.IsNullOrWhiteSpace(paramValue))
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+    }
+}
