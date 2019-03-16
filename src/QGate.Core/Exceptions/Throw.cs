b45b0d1b56b743d69af0d4021ccd4476d7f8c569
filip.kovar <filip,kovar@dev.com>
2019-03-16 21:15:42 +0100

    Added nested property validation and message

--- src/QGate.Core/Exceptions/Throw.cs
+++ src/QGate.Core/Exceptions/Throw.cs
@@ -1,23 +1,46 @@
-﻿using System;
+﻿using QGate.Core.Collections;
+using System;
+using System.Collections.Generic;
 
 namespace QGate.Core.Exceptions
 {
     public static class Throw
     {
-        public static void IfNull(object paramValue, string paramName)
+        public static void IfNull(object paramValue, params string[] paramPath)
         {
             if (paramValue == null)
             {
-                throw new ArgumentNullException(paramName);
+                throw new ArgumentNullException(GetFormattedParamPath(paramPath));
             }
         }
 
-        public static void IfNullOrWhiteSpace(string paramValue, string paramName)
+        public static void IfNullOrWhiteSpace(string paramValue, params string[] paramPath)
         {
             if (string.IsNullOrWhiteSpace(paramValue))
             {
-                throw new ArgumentNullException(paramName);
+                throw new ArgumentNullException(GetFormattedParamPath(paramPath));
             }
+        }
+
+        public static void IfNullOrEmpty<T>(IEnumerable<T> paramValue, params string[] paramPath)
+        {
+            var formattedParamPath = GetFormattedParamPath(paramPath);
+            IfNull(paramValue, formattedParamPath);
+
+            if(paramValue.IsNullOrEmpty())
+            {
+                throw new ArgumentNullException(formattedParamPath, "Collection is empty");
+            }
+        }
+
+        private static string GetFormattedParamPath(params string[] paramPath)
+        {
+            if(paramPath.IsNullOrEmpty())
+            {
+                return "unknown parameter.";
+            }
+
+            return string.Join(".", paramPath);
         }
     }
 }
