4e56591a533c88a5dc5c34fe34a3ba83d281735a
filip.kovar <filip.kovar>
2019-05-07 21:51:12 +0200

    Added Type Extensions

---
+++ src/QGate.Core/Types/TypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using QGate.Core.Exceptions;
+using System;
+using System.Collections;
+
+namespace QGate.Core.Types
+{
+    public static class TypeExtensions
+    {
+        private static Type _enumerableType = typeof(IEnumerable);
+
+        public static bool IsCollection(this Type type)
+        {
+            Throw.IfNull(type, nameof(type));
+            return _enumerableType.IsAssignableFrom(type);
+        }
+    }
+}
