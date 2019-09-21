df76b5bb8656d0ba741ab9f3e27fb78e3369f2fb
filip.kovar <filip.kovar>
2019-09-21 18:07:35 +0200

    String extensions created

---
+++ src/QGate.Core/Texts/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace QGate.Core.Texts
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Determines (safely) whether are string equals with ignore case comparison
+        /// </summary>
+        /// <param name="a">Can be null</param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool EqualsIgnoreCase(this string a, string b)
+        {
+            return string.Equals(a, b, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
